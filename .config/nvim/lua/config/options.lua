6e7c763db77a701ecfefdd3a840f7ad67b57753a
zekiz774 <gerlinge122@gmail.com>
2024-03-05 16:44:48 +0100

    backup

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.opt.spelllang = "en_us,de"
+vim.opt.spell = true
+
+vim.opt.textwidth = 80
+vim.opt.linebreak = true
+vim.opt.wrap = true
--- .config/nvim/lua/plugins/obsidan.lua
+++ .config/nvim/lua/plugins/obsidan.lua
@@ -28,15 +28,56 @@ return {
     },
 
     notes_subdir = "notes",
+    new_notes_location = "notes_subdir",
 
     daily_notes = {
       folder = "notes/daily",
-      template = "daily-template",
+      template = "daily-template.md",
     },
 
     templates = {
       subdir = "templates",
     },
+
+    -- Optional, customize how names/IDs for new notes are created.
+    note_id_func = function(title)
+      -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+      -- In this case a note with the title 'My new note' will be given an ID that looks
+      -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+      local suffix = ""
+      if title ~= nil then
+        -- If title is given, transform it into valid file name.
+        suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", "")
+      else
+        -- If title is nil, just add 4 random uppercase letters to the suffix.
+        for _ = 1, 4 do
+          suffix = suffix .. string.char(math.random(65, 90))
+        end
+      end
+      return tostring(suffix)
+    end,
+
+    -- Optional, alternatively you can customize the frontmatter data.
+    ---@return table
+    note_frontmatter_func = function(note)
+      -- Add the title of the note as an alias.
+      if note.title then
+        note:add_alias(note.title)
+      end
+
+      local out = { aliases = note.aliases, tags = note.tags }
+
+      -- `note.metadata` contains any manually added fields in the frontmatter.
+      -- So here we just make sure those fields are kept in the frontmatter.
+      if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+        for k, v in pairs(note.metadata) do
+          out[k] = v
+        end
+      end
+
+      return out
+    end,
+
     mappings = {
       -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
 
@@ -66,6 +107,14 @@ return {
       ["<leader>fd"] = {
         action = function()
           return vim.cmd("ObsidianToday")
+        end,
+        opts = { noremap = true, silent = true },
+      },
+
+      -- Creates a new note
+      ["<leader>fn"] = {
+        action = function()
+          return vim.cmd("ObsidianNew")
         end,
         opts = { noremap = true, silent = true },
       },
