7c59e5cf6df6afd31000c1a4ad43cf3117fdd04a
zekiz <gerlinge122@gmail.com>
2026-01-06 19:42:10 +0100

    fixed audio of gs.sh

--- .local/bin/gs.sh
+++ .local/bin/gs.sh
@@ -7,8 +7,11 @@ CURRENT_SINK=$(pactl get-default-sink)
 CURRENT_VOLUME=$(pactl -f json get-sink-volume $CURRENT_SINK | jq '.volume["front-left"].value')
 
 MONITOR_SINK=alsa_output.pci-0000_03_00.1.hdmi-stereo-extra3
-pactl set-default-sink $MONITOR_SINK
-# pactl set-sink-volume $MONITOR_SINK 100%
+(
+  sleep 1
+  pactl set-default-sink $MONITOR_SINK
+) &
+pactl set-sink-volume $MONITOR_SINK 150%
 
 # start gamescope
 ENABLE_HDR_WSI=1 gamescope --fullscreen -w 3840 -h 2160 --hdr-enabled --hdr-debug-force-output --hdr-sdr-content-nits 600 --mangoapp -- env ENABLE_GAMESCOPE_WSI=1 DXVK_HDR=1 DISABLE_HDR_WSI=1 steam -tenfoot
