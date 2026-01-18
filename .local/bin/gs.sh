589a2d394361c4f42eb42e1dfbd362fb59e3095b
zekiz <gerlinge122@gmail.com>
2026-01-18 15:31:07 +0100

    added oss-cad-suite to zprofile path

--- .local/bin/gs.sh
+++ .local/bin/gs.sh
@@ -20,26 +20,25 @@ CURRENT_SINK=$(pactl get-default-sink)
 CURRENT_VOLUME=$(pactl -f json get-sink-volume $CURRENT_SINK | jq '.volume["front-left"].value')
 
 # Turn on TV with a remote raspberrypi with cec-ctl installed. SSH key needs to be set up already
-(
-  ssh raspberrypi@raspberrypi <<'ENDSSH'
+ssh raspberrypi@raspberrypi <<'ENDSSH'
 cec-ctl --to TV --playback --image-view-on 
 sleep 2
 cec-ctl --to TV --active-source phys-addr=4.0.0.0 
 ENDSSH
-) &
 
-#
+# sets audio device and volume
+VOLUME=120%
 MONITOR_SINK=alsa_output.pci-0000_03_00.1.hdmi-stereo-extra3
 (
   sleep 1
   pactl set-default-sink $MONITOR_SINK
-  pactl set-sink-volume $MONITOR_SINK 150%
+  pactl set-sink-volume $MONITOR_SINK $VOLUME
 
 ) &
 
 # start gamescope
 pkill steam
-ENABLE_HDR_WSI=1 gamescope --fullscreen -w 3840 -h 2160 --hdr-enabled --hdr-debug-force-output --hdr-sdr-content-nits 600 --mangoapp -e -- env ENABLE_GAMESCOPE_WSI=1 DXVK_HDR=1 DISABLE_HDR_WSI=1 steam -tenfoot
+ENABLE_HDR_WSI=1 gamescope --fullscreen -w 3840 -h 2160 --hdr-enabled --hdr-debug-force-output --hdr-sdr-content-nits 600 --mangoapp --adaptive-sync -f -e -- env ENABLE_GAMESCOPE_WSI=1 DXVK_HDR=1 DISABLE_HDR_WSI=1 steam -tenfoot
 
 # reset after gs closed
 
--- .zprofile
+++ .zprofile
@@ -1,4 +1,4 @@
 export ANDROID_HOME=$HOME/Android/Sdk
 export ANDROID_AVD_HOME=$HOME/.config/.android/avd
-export PATH=${PATH}:${HOME}/.local/bin:${HOME}/.cargo/bin:${ANDROID_HOME}/emulator:${ANDROID_HOME}/platform-tools:${ANDROID_HOME}/cmdline-tools/latest/bin:/opt/flutter/bin
+export PATH=${PATH}:${HOME}/.local/bin:${HOME}/.cargo/bin:${ANDROID_HOME}/emulator:${ANDROID_HOME}/platform-tools:${ANDROID_HOME}/cmdline-tools/latest/bin:/opt/flutter/bin:/opt/oss-cad-suite/bin
 
